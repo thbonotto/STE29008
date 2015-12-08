a6119c45287ffe5626aa5466b999457c5514f08a
Thiago Bonotto <thbonotto@gmail.com>
2015-12-08 09:23:34 -0200

    Desenvolvimento do timer.

--- Lab07/src/Main.cpp
+++ Lab07/src/Main.cpp
@@ -1,15 +1,33 @@
+#include "NTC.h"
+#include "ADConverter.h"
+#include "Timer.h"
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#include "NTC.h"
-#include "ADConverter.h"
+static const unsigned long long FREQ = 16000000/256; //Hz
+static const unsigned long long PERIOD = 16; // ms
+
+void adc(){
+	ADConverter adc;
+	    NTC sensor;
+	    while(1) {
+	    	sensor.getTemperature(adc.getBuffer().pop());
+	    }
+}
+
+void timer(){
+	Timer timer(FREQ);
+	DDRB = 0xFF;
+	sei();
+	while(true){
+		timer.restart();
+		while(timer.microseconds() < 500000);
+		PORTB = ~ PORTB;
+	}
+}
 
 int main(void) {
-    ADConverter adc;
-    NTC sensor;
-    while(1) {
-    	sensor.getTemperature(adc.getBuffer().pop());
-    }
     return 0;
 }
 
